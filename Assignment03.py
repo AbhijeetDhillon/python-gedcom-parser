4d8c8c5d2b0b2fa0b3f0f7344ef7701b22bb5543
aishusathyakumar <aishwarya@techindia.com>
2020-02-17 19:39:00 -0500

    Update Assignment03.py

--- Assignment03.py
+++ Assignment03.py
@@ -1,30 +1,58 @@
-
 # Dictionary
-li = {"0": ["INDI", "FAM", "HEAD", "TRLR", "NOTE"], "1": ["NAME", "SEX", "BIRT","DEAT", "FAMC", "FAMS", "MARR", "HUSB", "WIFE", "CHIL", "DIV"], "2": ["DATE"]}
+li = {"0": ["INDI", "FAM", "HEAD", "TRLR", "NOTE"], "1": ["NAME", "SEX", "BIRT",
+                                                          "DEAT", "FAMC", "FAMS", "MARR", "HUSB", "WIFE", "CHIL", "DIV"], "2": ["DATE"]}
+
 
 ind_details = {}
 
 # gedcom parser
-def file_reading_gen(path, sep):
+
+
+def initialize_var():
+    individual_id = ""
+    name = ""
+    sex = ""
+    birt = ""
+    deat = ""
+    # famc = []
+    # fams = []
+    fam = ""
+    marr = ""
+    husb = ""
+    wife = ""
+    chil = ""
+    div = ""
+    date = ""
+    fs = 1
+    fc = 1
+    # idcount = 0
+
+
+def file_reading_gen(path):
     file = open(path, "r")
+    individual_id = ""
+    name = ""
+    sex = ""
+    birt = ""
+    deat = ""
+    fs = 1
+    fc = 1
+    famc = {}
+    fams = {}
+    fam = ""
+    marr = ""
+    husb = ""
+    wife = ""
+    chil = ""
+    div = ""
+    date = ""
+    idcount = 0
+    # initialize_var()
     for line in file:
         # print("-->",line.strip("\n"))
         liner = line.split()
-        individual_id = ""
-        name = ""
-        sex = ""
-        birt = ""
-        deat = ""
-        famc = []
-        fams = []
-        fam = ""
-        marr = ""
-        husb = ""
-        wife = ""
-        chil = ""
-        div = ""
-        date = ""
-        idcount=0
+        #print(liner)
+
         if liner[0] in ["0", "1", "2"]:
             for key, values in li.items():
 
@@ -34,25 +62,27 @@ def file_reading_gen(path, sep):
                         # print(' '.join(liner[2::]))
                         if liner[1] == "NAME":
                             name = (' '.join(liner[2::]))
-                            print(name)
+                            # print(name)
                         elif liner[1] == "SEX":
                             sex = (' '.join(liner[2::]))
-                            print(sex)
+                            # print(sex)
                         elif liner[1] == "FAMS":
-                            fams.append(' '.join(liner[2::]))
-                            print(fams)
+                            fams[fs]=(' '.join(liner[2::]))
+                            fs = fs + 1
+                            #print(fams)
                         elif liner[1] == "FAMC":
-                            famc.append(' '.join(liner[2::]))
-                            print(famc)
+                            fams[fc]=(' '.join(liner[2::]))
+                            fc = fc + 1
+                            #print(famc)
                         elif liner[1] == "HUSB":
                             husb = ' '.join(liner[2::])
-                            print(husb)
+                            # print(husb)
                         elif liner[1] == "WIFE":
                             wife = ' '.join(liner[2::])
-                            print(wife)
+                            # print(wife)
                         elif liner[1] == "CHIL":
                             chil = ' '.join(liner[2::])
-                            print(chil)
+                            # print(chil)
                         elif liner[1] == "BIRT":
                             bcount = True
                         elif liner[1] == "DEAT":
@@ -64,13 +94,13 @@ def file_reading_gen(path, sep):
                         elif liner[1] == "DATE":
                             if bcount == True:
                                 birt = ' '.join(liner[2::])
-                            elif dcount == True:
+                            if dcount == True:
                                 deat = ' '.join(liner[2::])
-                            elif marrcount == True:
+                            if marrcount == True:
                                 marr = ' '.join(liner[2::])
-                            elif divcount == True:
+                            if divcount == True:
                                 div = ' '.join(liner[2::])
-                            print(birt,deat,marr,div)
+                            # print(birt, deat, marr, div)
 
                     elif len(liner) > 2 and (liner[2] in ["INDI", "FAM"]):
                         # pass
@@ -81,11 +111,16 @@ def file_reading_gen(path, sep):
                         divcount = False
                         # print(liner)
                         if liner[2] == "INDI":
-                            # if idcount == "1":
-                            # print(name)
-                            ind_details[id] = {"ID":individual_id,"Name":name,'Gender':sex,'Birthday':birt,'Death':deat,'FAMS':fams,'FAMC':famc}
+                            if idcount == 1:
+                                ind_details[individual_id] = {"Name": name, 'Gender': sex, 'Birthday': birt, 'Death': deat, 'FAMS': fams, 'FAMC': famc}
+                            #print(ind_details,"----------------------------------------------")
+                            initialize_var()
+                            fams={}
+                            famc={}
+                            fs = 1
+                            fc = 1
                             individual_id = liner[1]
-                            # print(individual_id)
+                            #print(individual_id)
                             idcount = 1
                         else:
                             fam = liner[1]
@@ -98,7 +133,7 @@ def file_reading_gen(path, sep):
             pass
 
 
-
 # function calling
-file_reading_gen("D:\Assgnments\CS 555\Assignment 3\Family.ged", "|")
+file_reading_gen("Family.ged")
 print(ind_details)
+
